1335989302fd03672283371d055f6320acecff22
truonganletk <truonganletk@gmail.com>
2022-03-15 01:01:17 +0300

    show welcome screen first time on app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 // @dart=2.9
 
+import 'package:after_layout/after_layout.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:puresty/authwrapper.dart';
+import 'package:puresty/screens/welcome_screen/welcome_screen.dart';
 import 'package:puresty/services/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'services/firebase_options.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 
@@ -32,6 +35,16 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      color: Colors.blue,
+      home: new Splash(),
+    );
+  }
+}
+
+class AppProvider extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -57,5 +70,34 @@ class MyApp extends StatelessWidget {
         home: AuthenticationWrapper(),
       ),
     );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  SplashState createState() => new SplashState();
+}
+
+class SplashState extends State<Splash> with AfterLayoutMixin<Splash> {
+  Future checkFirstSeen() async {
+    FlutterNativeSplash.remove();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (!_seen) {
+      await prefs.setBool('seen', true);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WelcomeScreen()),
+      );
+    }
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) => checkFirstSeen();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppProvider();
   }
 }
