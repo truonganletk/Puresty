8312737bc291d7b02d7b5c040cc73099d709b1c1
truonganletk <truonganletk@gmail.com>
2022-03-24 22:11:00 +0300

    responsive post screen

--- lib/screens/main_screens/post_screen/post_screen.dart
+++ lib/screens/main_screens/post_screen/post_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:puresty/constants/app_colors.dart';
+import 'package:puresty/constants/size_config.dart';
 import 'package:puresty/models/post.dart';
 import 'package:puresty/screens/main_screens/post_screen/post_detail_screen/post_detail_screen.dart';
 
@@ -65,134 +66,146 @@ class _PostScreenState extends State<PostScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          physics: NeverScrollableScrollPhysics(),
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Post",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: black,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w900,
-                        ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return OrientationBuilder(builder: (context, orientation) {
+        SizeConfig().init(constraints, orientation);
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Container(
+            padding: EdgeInsets.fromLTRB(
+                2.64 * SizeConfig.heightMultiplier,
+                4.64 * SizeConfig.heightMultiplier,
+                2.64 * SizeConfig.heightMultiplier,
+                2.64 * SizeConfig.heightMultiplier),
+            child: SingleChildScrollView(
+              physics: NeverScrollableScrollPhysics(),
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Post",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: black,
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: 630,
-                        child: ListView.builder(
-                            controller: scrollController,
-                            itemCount: _allresultList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => PostDetailScreen(
-                                            post: _listpost.elementAt(index))),
-                                  );
-                                },
-                                child: Stack(
-                                  children: [
-                                    Card(
-                                      margin:
-                                          EdgeInsets.symmetric(vertical: 10),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(15.0)),
-                                        child: ShaderMask(
-                                          shaderCallback: (rect) =>
-                                              LinearGradient(
-                                            begin: Alignment.bottomCenter,
-                                            end: Alignment.center,
-                                            colors: [
-                                              Color.fromARGB(255, 58, 58, 58),
-                                              Colors.transparent,
-                                            ],
-                                          ).createShader(rect),
-                                          blendMode: BlendMode.darken,
-                                          child: Container(
-                                            height: 229,
-                                            width: double.infinity,
-                                            decoration: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(15.0),
-                                                image: DecorationImage(
-                                                  fit: BoxFit.cover,
-                                                  image: AssetImage(
-                                                      'assets/images/posttemplateimage.jpg'),
-                                                  colorFilter: ColorFilter.mode(
-                                                      Colors.black26,
-                                                      BlendMode.darken),
-                                                )),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 86.77 * SizeConfig.heightMultiplier,
+                            child: ListView.builder(
+                                controller: scrollController,
+                                itemCount: _allresultList.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                PostDetailScreen(
+                                                    post: _listpost
+                                                        .elementAt(index))),
+                                      );
+                                    },
+                                    child: Stack(
+                                      children: [
+                                        Card(
+                                          margin: EdgeInsets.symmetric(
+                                              vertical: 10),
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(15.0)),
+                                            child: ShaderMask(
+                                              shaderCallback: (rect) =>
+                                                  LinearGradient(
+                                                begin: Alignment.bottomCenter,
+                                                end: Alignment.center,
+                                                colors: [
+                                                  Color.fromARGB(
+                                                      255, 58, 58, 58),
+                                                  Colors.transparent,
+                                                ],
+                                              ).createShader(rect),
+                                              blendMode: BlendMode.darken,
+                                              child: Container(
+                                                height: 32.57 *
+                                                    SizeConfig.heightMultiplier,
+                                                width: double.infinity,
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            15.0),
+                                                    image: DecorationImage(
+                                                      fit: BoxFit.cover,
+                                                      image: AssetImage(
+                                                          'assets/images/posttemplateimage.jpg'),
+                                                      colorFilter:
+                                                          ColorFilter.mode(
+                                                              Colors.black26,
+                                                              BlendMode.darken),
+                                                    )),
+                                              ),
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ),
-                                    Positioned(
-                                      bottom: 20,
-                                      left: 15,
-                                      child: Container(
-                                        padding:
-                                            EdgeInsets.symmetric(horizontal: 7),
-                                        width: 300,
-                                        child: Row(
-                                          children: [
-                                            Expanded(
-                                              child: Text(
-                                                  _listpost
-                                                      .elementAt(index)
-                                                      .title,
-                                                  style: TextStyle(
-                                                    fontFamily: 'Poppins',
-                                                    color: white,
-                                                    fontSize: 24,
-                                                    fontWeight: FontWeight.w700,
-                                                    fontStyle: FontStyle.normal,
-                                                  )),
+                                        Positioned(
+                                          bottom: 20,
+                                          left: 15,
+                                          child: Container(
+                                            padding: EdgeInsets.symmetric(
+                                                horizontal: 7),
+                                            width: 76.34 *
+                                                SizeConfig.widthMultiplier,
+                                            child: Row(
+                                              children: [
+                                                Expanded(
+                                                  child: Text(
+                                                      _listpost
+                                                          .elementAt(index)
+                                                          .title,
+                                                      style: TextStyle(
+                                                        fontFamily: 'Poppins',
+                                                        color: white,
+                                                        fontSize: 24,
+                                                        fontWeight:
+                                                            FontWeight.w700,
+                                                        fontStyle:
+                                                            FontStyle.normal,
+                                                      )),
+                                                ),
+                                              ],
                                             ),
-                                          ],
+                                          ),
                                         ),
-                                      ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                                // Container(
-                                //   width: 300,
-                                //   height: 100,
-                                //   color: Colors.pinkAccent,
-                                // child: Center(child: Text('<3')),
-                                //),
-                              );
-                            }),
-                      )
-                    ],
-                  ),
+                                  );
+                                }),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        );
+      });
+    });
   }
 }
